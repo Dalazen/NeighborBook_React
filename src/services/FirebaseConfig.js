631cb7b8ac97cc5912b5dde0ae3e88a90310a77f
Guilherme Dalazen <guilhermebatistellad@gmail.com>
2020-11-30 20:48:22 -0300

    Liberando acesso ao firebase para o professor

---
+++ src/services/FirebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD7LSjAmDTTY5vxJoXWFaWB-8MOYUJOkEw",
+    authDomain: "neighborbook-c514a.firebaseapp.com",
+    databaseURL: "https://neighborbook-c514a.firebaseio.com",
+    projectId: "neighborbook-c514a",
+    storageBucket: "neighborbook-c514a.appspot.com",
+    messagingSenderId: "628220628411",
+    appId: "1:628220628411:web:f661993e2508b03cb7c89e"
+};
+
+export default firebaseConfig;
